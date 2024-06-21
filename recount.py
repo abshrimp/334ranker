200f81835e387cdd32661a1f7e5e6dab3ad243e4
AB <128656365+abshrimp@users.noreply.github.com>
2024-06-22 06:40:20 +0900

    Update recount.py

--- recount.py
+++ recount.py
@@ -205,6 +205,8 @@ def login_twitter(account, password, tel, driver):
             element_account.send_keys(Keys.ENTER)
             time.sleep(20)
 
+            driver.save_screenshot("a.png")
+
             elcount = 1
             element_tel = driver.find_elements(By.TAG_NAME, "input")   
             if len(element_tel) < 2:   
@@ -214,9 +216,12 @@ def login_twitter(account, password, tel, driver):
                     act.send_keys(tel[i])
                     act.perform()
                 time.sleep(2)
+                driver.save_screenshot("b.png")
                 element_tel[0].send_keys(Keys.ENTER)
                 elcount = 0
                 time.sleep(20)
+
+            driver.save_screenshot("c.png")
                 
             element_pass = driver.find_elements(By.TAG_NAME, "input")[elcount]      
             for i in range(len(password)):          
@@ -226,6 +231,8 @@ def login_twitter(account, password, tel, driver):
             time.sleep(2)
             element_pass.send_keys(Keys.ENTER)
             time.sleep(20)
+
+            driver.save_screenshot("d.png")
 
             driver.get('https://x.com/home')
             time.sleep(20)
